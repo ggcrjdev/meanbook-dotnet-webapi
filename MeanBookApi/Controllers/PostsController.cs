544b67a13a4d947309b429d34ffe00ab278913f8
gustavo.gomes <gustavo.gomesc@gmail.com>
2016-10-25 18:50:24 -0200

    Removed unnecessary namespaces.

--- MeanBookApi/Controllers/PostsController.cs
+++ MeanBookApi/Controllers/PostsController.cs
@@ -2,9 +2,6 @@
 using MeanBookApi.Models;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
 using System.Web.Http;
 
 namespace MeanBookApi.Controllers
